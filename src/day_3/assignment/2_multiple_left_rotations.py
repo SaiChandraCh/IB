69087a30c06e14f90dcda02123349b222585ff51
SaiChandraCh <saichandrachilupuri@gmail.com>
2019-08-03 12:08:44 +0530

    solved 2_multiple_left_rotations.py

--- src/day_3/assignment/2_multiple_left_rotations.py
+++ src/day_3/assignment/2_multiple_left_rotations.py
@@ -5,10 +5,12 @@ class Solution:
     def solve(self, A, B):
         result = []
         for i in B:
-            a = A
-            while i >= 0:
+            a = A[:]
+            while i > 0:
                 temp = a.pop(0)
                 a.append(temp)
                 i -= 1
             result.append(a)
         return result
+if __name__ == '__main__':
+    print(Solution.solve(Solution,[1, 2, 3, 4, 5],[2,3]))
